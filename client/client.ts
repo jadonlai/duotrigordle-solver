d8baafd2ca7cef4eea6b66f734193dd58218a5b4
jadonlai <jlai43@calpoly.edu>
2024-05-30 20:23:11 -0700

    added best board selection based on number of possible solutions

--- client/client.ts
+++ client/client.ts
@@ -1,23 +1,31 @@
-import assert from 'assert';
-import puppeteer from 'puppeteer';
-import { Page } from 'puppeteer';
-import swipl from 'swipl';
-import { term } from 'swipl';
+import assert from "assert";
+import puppeteer from "puppeteer";
+import { Page } from "puppeteer";
+import swipl from "swipl";
+import { term } from "swipl";
+import { WORDS_TARGET } from "./wordlist";
 const { list, compound, variable, serialize } = term;
 
-const CHANGELOG_CLOSE_SELECTOR = '._modal_y9oz3_1 > ._button_1xh0d_1';
-const PRACTICE_TAB_SELECTOR = 'div._tabs_5us1c_1 > div:nth-child(2) > button';
-const PRACTICE_LINK_SELECTOR = 'div._tabWrapper_ag3fe_24 > div._tabContainer_ag3fe_30._shown_ag3fe_36 > div:nth-child(1) > button';
-const MAIN_SELECTOR = '._main_kv0wd_1';
-const BOARDS_SELECTOR = 'div._board_1277w_1';
-const CELL_SELECTOR = 'div._cell_1277w_56';
+const CHANGELOG_CLOSE_SELECTOR = "._modal_y9oz3_1 > ._button_1xh0d_1";
+const PRACTICE_TAB_SELECTOR = "div._tabs_5us1c_1 > div:nth-child(2) > button";
+const PRACTICE_LINK_SELECTOR =
+  "div._tabWrapper_ag3fe_24 > div._tabContainer_ag3fe_30._shown_ag3fe_36 > div:nth-child(1) > button";
+const MAIN_SELECTOR = "._main_kv0wd_1";
+const BOARDS_SELECTOR = "div._board_1277w_1";
+const CELL_SELECTOR = "div._cell_1277w_56";
 
 async function gather_results(page: Page): Promise<string[]> {
   const boards = await page.$$(BOARDS_SELECTOR);
-  const resultsPromises = boards.map(async board => {
-    const cells = await board.$$eval(CELL_SELECTOR, els => els.map(el => el.className));
+  const resultsPromises = boards.map(async (board) => {
+    const cells = await board.$$eval(CELL_SELECTOR, (els) =>
+      els.map((el) => el.className)
+    );
     const result = cells.slice(-10, -5);
-    return result.map(cls => cls.includes("green") ? "f" : cls.includes("yellow") ? "p" : "i").join('');
+    return result
+      .map((cls) =>
+        cls.includes("green") ? "f" : cls.includes("yellow") ? "p" : "i"
+      )
+      .join("");
   });
 
   const results = await Promise.all(resultsPromises);
@@ -27,7 +35,32 @@ async function gather_results(page: Page): Promise<string[]> {
 
 function solvedAmount(board: string[]): number {
   const solve = board[board.length - 1];
-  return (solve.match(/f/g) ?? []).length + 0.5 * (solve.match(/p/g) ?? []).length;
+  return (
+    (solve.match(/f/g) ?? []).length + 0.5 * (solve.match(/p/g) ?? []).length
+  );
+}
+
+function possibleSolutions(board, words) {
+  return words.filter((word) => {
+    return board.every((result) => {
+      return isValidSolution(word, result);
+    });
+  });
+}
+
+function isValidSolution(word, result) {
+  for (let i = 0; i < word.length; i++) {
+    if (
+      (result[i] === "f" && word[i] !== result[i]) ||
+      (result[i] === "p" &&
+        word.includes(result[i]) &&
+        word[i] !== result[i]) ||
+      (result[i] === "i" && word.includes(result[i]))
+    ) {
+      return false;
+    }
+  }
+  return true;
 }
 
 (async () => {
@@ -36,7 +69,7 @@ function solvedAmount(board: string[]): number {
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto('https://duotrigordle.com/game/practice/normal');
+  await page.goto("https://duotrigordle.com/game/practice/normal");
 
   await page.waitForSelector(CHANGELOG_CLOSE_SELECTOR);
   await page.click(CHANGELOG_CLOSE_SELECTOR);
@@ -44,25 +77,50 @@ function solvedAmount(board: string[]): number {
 
   await page.waitForSelector(PRACTICE_LINK_SELECTOR);
   // for whatever reason the events take a sec to get hooked up
-  await new Promise(resolve => setTimeout(resolve, 150))
+  await new Promise((resolve) => setTimeout(resolve, 150));
   await page.click(PRACTICE_LINK_SELECTOR);
 
-  await page.type(MAIN_SELECTOR, 'TARES');
-  await page.keyboard.press('Enter');
+  await page.type(MAIN_SELECTOR, "TARES");
+  await page.keyboard.press("Enter");
 
-  swipl.call('[entropy]');
+  swipl.call("[entropy]");
 
-  const guesses = ['TARES'];
-  const all_results = (await gather_results(page)).map(r => [r]);
+  const guesses = ["TARES"];
+  const all_results = (await gather_results(page)).map((r) => [r]);
   while (true) {
-    const first_board = all_results.reduce((best_board, board) => solvedAmount(board) > solvedAmount(best_board) && solvedAmount(board) !== 5 ? board : best_board);
+    const first_board = all_results.reduce((best_board, board) => {
+      const solvedAmountBoard = solvedAmount(board);
+      const possibleSolutionsBoard = possibleSolutions(
+        board,
+        WORDS_TARGET
+      ).length;
+
+      const solvedAmountBestBoard = solvedAmount(best_board);
+      const possibleSolutionsBestBoard = possibleSolutions(
+        best_board,
+        WORDS_TARGET
+      ).length;
+
+      if (
+        solvedAmountBoard > solvedAmountBestBoard &&
+        solvedAmountBoard !== 5
+      ) {
+        return board;
+      } else if (solvedAmountBoard === solvedAmountBestBoard) {
+        if (possibleSolutionsBoard < possibleSolutionsBestBoard) {
+          return board;
+        }
+      }
+      return best_board;
+    }, all_results[0]);
 
     const query = serialize(
-      compound('max_entropies_given', [
+      compound("max_entropies_given", [
         list(guesses),
         list(first_board),
-        variable('ME'),
-      ]));
+        variable("ME"),
+      ])
+    );
     console.log("querying prolog:", query);
     const ret = swipl.call(query);
     console.log(ret);
@@ -73,14 +131,13 @@ function solvedAmount(board: string[]): number {
     }
 
     await page.type(MAIN_SELECTOR, ret.ME);
-    await page.keyboard.press('Enter');
+    await page.keyboard.press("Enter");
 
     guesses.push(ret.ME);
 
     const results = await gather_results(page);
-    all_results.map((acc, i) => acc.push(results[i]))
+    all_results.map((acc, i) => acc.push(results[i]));
   }
-
 
   //await browser.close();
 })();
